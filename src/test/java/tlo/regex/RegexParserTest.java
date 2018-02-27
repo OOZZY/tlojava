1de53b732e8f1b1064cd45c3edf867ab002c6f43
oozzy <oozzy.tlo@gmail.com>
2018-02-27 11:05:06 -0800

    Add tests for cases where parsing should fail

--- src/test/java/tlo/regex/RegexParserTest.java
+++ src/test/java/tlo/regex/RegexParserTest.java
@@ -3,6 +3,7 @@ package tlo.regex;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,15 +12,6 @@ import org.junit.Test;
 public class RegexParserTest {
   private static final Logger logger = LogManager
       .getLogger(RegexParserTest.class);
-
-  public void assertParseSucceeds(RegexParser parser, String pattern) {
-    Regex regex = parser.parse(pattern);
-    assertEquals(regex.toRegexString(), pattern);
-    if (logger.isDebugEnabled()) {
-      logger.debug(regex);
-      logger.debug(regex.toRegexString());
-    }
-  }
 
   @Test
   public void testParsing() {
@@ -31,6 +23,38 @@ public class RegexParserTest {
     assertParseSucceeds(parser, "(a|b)c(f?)+.*");
     assertParseSucceeds(parser, "((\\+)*)?");
     assertParseSucceeds(parser, "((\\\\+)*)?");
+  }
+
+  public void assertParseSucceeds(RegexParser parser, String pattern) {
+    Regex regex = parser.parse(pattern);
+    assertEquals(regex.toRegexString(), pattern);
+    if (logger.isDebugEnabled()) {
+      logger.debug(regex);
+      logger.debug(regex.toRegexString());
+    }
+  }
+
+  @Test
+  public void testParsingFailure() {
+    RegexParser parser = new RegexParser();
+    assertParseFails(parser, "");
+    assertParseFails(parser, "*");
+    assertParseFails(parser, "(a");
+    assertParseFails(parser, "a|");
+    assertParseFails(parser, "\\a");
+  }
+
+  public void assertParseFails(RegexParser parser, String pattern) {
+    try {
+      parser.parse(pattern);
+      fail();
+    } catch (Exception exception) {
+      if (logger.isDebugEnabled()) {
+        logger.debug("Error message when parsing \"{}\": {}", pattern,
+            exception.getMessage());
+      }
+      return;
+    }
   }
 
   @Test
